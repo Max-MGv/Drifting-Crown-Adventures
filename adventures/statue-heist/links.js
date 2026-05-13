d08e456d2500484f4a326b6cea5845cedd5ef60c
Max <max.mghvdliashvili@gmail.com>
2026-05-13 15:39:28 +0400

    Admin: update image config for statue-heist

--- adventures/statue-heist/links.js
+++ adventures/statue-heist/links.js
@@ -1,11 +1,68 @@
 window.AdventureLinks = {
-  'drifting-crown':     { url: '', credit: '' },
-  'magisters-district': { url: '', credit: '' },
-  'bakery':             { url: '', credit: '' },
-  'village-tailor':     { url: '', credit: '' },
-  'magic-popup':        { url: '', credit: '' },
-  'museum-study':       { url: '', credit: '' },
-  'boss-room':          { url: '', credit: '' },
-  'boss-room-battlemap':{ url: '', credit: '' },
-  'traveling':          { url: '', credit: '' },
+  "locations-drifting-crown-jpg": {
+    url: "https://www.czepeku.com/fantasy/scenes/sky-islands",
+    credit: "Cze & Peku",
+    lowres: true,
+    lowresSrc: "images/locations/drifting-crown-lowres.jpg?v=1778671947902"
+  },
+  "locations-village-tailor-jpg": {
+    url: "https://www.czepeku.com/fantasy/scenes/village-tailor",
+    credit: "Cze & Peku",
+    lowres: true,
+    lowresSrc: "images/locations/village-tailor-lowres.jpg?v=1778671966234"
+  },
+  "locations-magisters-district-jpg": {
+    url: "https://www.czepeku.com/fantasy/scenes/magisters-market",
+    credit: "Cze & Peku",
+    lowres: true,
+    lowresSrc: "images/locations/magisters-district-lowres.jpg?v=1778671977209"
+  },
+  "locations-boss-room-jpg": {
+    url: "https://www.czepeku.com/fantasy/scenes/temple-of-the-medusa-throne",
+    credit: "Cze & Peku",
+    lowres: true,
+    lowresSrc: "images/locations/boss-room-lowres.jpg?v=1778671995022"
+  },
+  "locations-boss-room-battlemap-jpg": {
+    url: "https://www.czepeku.com/fantasy/maps/temple-of-the-medusa-throne/original",
+    credit: "Cze & Peku",
+    lowres: true,
+    lowresSrc: "images/locations/boss-room-battlemap-lowres.jpg?v=1778672014225"
+  },
+  "locations-magic-popup-jpg": {
+    url: "https://www.czepeku.com/fantasy/scenes/magic-popup-shop",
+    credit: "Cze & Peku",
+    lowres: true,
+    lowresSrc: "images/locations/magic-popup-lowres.jpg?v=1778672024402"
+  },
+  "locations-traveling-jpg": {
+    url: "https://www.czepeku.com/fantasy/scenes/medusas-wake",
+    credit: "Cze & Peku",
+    lowres: true,
+    lowresSrc: "images/locations/traveling-lowres.jpg?v=1778672043581"
+  },
+  "locations-bakery-jpg": {
+    url: "https://www.czepeku.com/fantasy/scenes/village-bakery",
+    credit: "Cze & Peku",
+    lowres: true,
+    lowresSrc: "images/locations/bakery-lowres.jpg?v=1778672057567"
+  },
+  "locations-local-shop-jpg": {
+    url: "https://www.czepeku.com/fantasy/scenes/badger-hill-shop",
+    credit: "Cze & Peku",
+    lowres: true,
+    lowresSrc: "images/locations/local-shop-lowres.jpg?v=1778672247931"
+  },
+  "locations-hunting-guild-jpg": {
+    url: "https://www.czepeku.com/fantasy/scenes/village-hunting-guild",
+    credit: "Cze & Peku",
+    lowres: true,
+    lowresSrc: "images/locations/hunting-guild-lowres.jpg?v=1778672254360"
+  },
+  "locations-museum-study-jpg": {
+    url: "https://www.czepeku.com/fantasy/maps/monster-genealogy-lab/original-day",
+    credit: "Cze & Peku",
+    lowres: true,
+    lowresSrc: "images/locations/museum-study-lowres.jpg?v=1778672358386"
+  }
 };
